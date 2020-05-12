9a85907398bf40ba52d5a7eb904d42a1fcaa85e1
Francesco Mattioli <Francesco.mttl@gmail.com>
2020-05-12 20:50:02 +0200

    Added recostruction

--- preprocessing/S65_R04.08.12_ICA_template.py
+++ preprocessing/S65_R04.08.12_ICA_template.py
@@ -7,34 +7,34 @@ from mne.channels import make_standard_montage
 from mne.preprocessing import ICA, corrmap
 import time
 import os
-#%%
 
-#Lista dei file raw
+# %%
+
+# Lista dei file raw
 raws = list()
-#Lista degli oggetti ica
+# Lista degli oggetti ica
 icas = list()
-#Lista dei soggetti
-subjects = [65,79,105,25,57]
-#Lista delle runs
-runs = [4,8,12]
-
+# Lista dei soggetti
+subjects = [65, 79, 105]
+# Lista delle runs
+runs = [4, 8, 12]
 
 for subj in subjects:
-    ls_run = [] #Lista dove inseriamo le run
+    ls_run = []  # Lista dove inseriamo le run
     for run in runs:
-        fname = eegbci.load_data(subj, runs=run)[0] #Prendo le run
-        raw_run = read_raw_edf(fname, preload=True) #Le carico
-        len_run = np.sum((raw_run._annotations).duration) #Controllo la durata
+        fname = eegbci.load_data(subj, runs=run)[0]  # Prendo le run
+        raw_run = read_raw_edf(fname, preload=True)  # Le carico
+        len_run = np.sum((raw_run._annotations).duration)  # Controllo la durata
         if len_run > 123:
-            raw_run.crop(tmax=124.4) #Taglio la parte finale
-        ls_run.append(raw_run) #Aggiungo la run alla lista delle run
+            raw_run.crop(tmax=124.4)  # Taglio la parte finale
+        ls_run.append(raw_run)  # Aggiungo la run alla lista delle run
     raw = concatenate_raws(ls_run)
-    eegbci.standardize(raw) #Cambio i nomi dei canali
-    montage = make_standard_montage('standard_1005') #Caricare il montaggio
-    raw.set_montage(montage) #Setto il montaggio
-    raw.filter(1.0, 79.0, fir_design='firwin', skip_by_annotation='edge') #Filtro
-    raw_notch = raw.notch_filter(freqs=60) #Faccio un filtro passa banda
-    raw.plot_psd(area_mode=None, show=False, average=False, fmin =1.0, fmax=80.0, dB=False, n_fft=160)
+    eegbci.standardize(raw)  # Cambio i nomi dei canali
+    montage = make_standard_montage('standard_1005')  # Caricare il montaggio
+    raw.set_montage(montage)  # Setto il montaggio
+    raw.filter(1.0, 79.0, fir_design='firwin', skip_by_annotation='edge')  # Filtro
+    raw_notch = raw.notch_filter(freqs=60)  # Faccio un filtro passa banda
+    raw.plot_psd(area_mode=None, show=False, average=False, fmin=1.0, fmax=80.0, dB=False, n_fft=160)
     # todo: qui salvare il plot psd
     # Ica
     ica = ICA(n_components=64, random_state=42, method="fastica", max_iter=1000)
@@ -49,33 +49,36 @@ for subj in subjects:
     raws.append(raw)
     icas.append(ica)
 
-#%%
+# %%
 eog_inds, eog_scores = icas[0].find_bads_eog(raws[0], ch_name='Fpz')
 icas[0].plot_properties(raws[0], picks=[], dB=False)
-exc_0 = [0,1,3,4,9,11,14,15,19,26,29,34,44,47,49,51,53,55,56,58,59,63,60,61]
-maybe = [ 7,12,23,24,40]
+exc_0 = [0, 1, 3, 4, 9, 11, 14, 15, 19, 26, 29, 34, 44, 47, 49, 51, 53, 55, 56, 58, 59, 63, 60, 61]
+maybe = [7, 12, 23, 24, 40]
 reconst_raw = raws[0].copy()
-icas[0].plot_overlay(reconst_raw, exclude=exc_65)
+# icas[0].plot_overlay(reconst_raw, exclude=exc_0)
 reconst_raw.plot_psd(area_mode=None, show=False, average=False, fmin=1.0, fmax=80.0, dB=False, n_fft=160)
 
-#%%
-exc_1 = [1,2,3]
+# %%
 
-ex_list = [exc_0,exc_1]
-
-#todo: Creiamo due cicli per
+ex_list = [exc_0]
 index_sub_temp = [0]
+
 for index in index_sub_temp:
-    print(index)
     for list_comp in ex_list:
-        print(list_comp)
         for comp in list_comp:
-            print(comp)
-            #corr_map = corrmap(icas, template=(index, comp), plot=False, threshold=0.85, label="artifact_" + str(index))
+            corr_map = corrmap(icas, template=(index, comp), plot=False, threshold=0.85, label="artifact_" + str(index))
+print([ica.labels_ for ica in icas])
 
 # todo: capire come fare questa corr map
 
-#%%
+icas[1].plot_properties(raws[1], dB=False, picks=icas[1].labels_["artifact_0"])
+icas[1].plot_components(picks=icas[1].labels_["artifact_0"])
 
-#todo: fare parte che riscostruisce tutti raw sulla base delle componenti escluse attraverso corr_map
-
+# %%
+# Ricostruisco
+reco_raws = []
+for index, ica in enumerate(icas):
+    reco_raw = raws[index].copy()
+    icas[index].exclude = icas[index].labels_["artifact_0"]
+    icas[index].apply(reco_raw)
+    reco_raws.append(reco_raw)
