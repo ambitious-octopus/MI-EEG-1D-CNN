e2898235a5e531e882e3ecedd9b61349cab790b7
Szara <dantoniosara2@gmail.com>
2020-05-20 17:51:13 +0200

    Editing of template real movement

--- preprocessing/S65_R03_07_11_ICA_template_real_movement.py
+++ preprocessing/S65_R03_07_11_ICA_template_real_movement.py
@@ -45,12 +45,16 @@ else:
 #%%
 #Lista dei file raw
 raws = list()
+
+raws_filtered = list()
 #Lista degli oggetti ica
 icas = list()
 #Lista dei soggetti
-subjects = [65]
+subjects = [66,23,31,46,109,95,38,102,77,12,57]
 #Lista delle runs
 runs = [3,7,11]
+
+ica_true = True
 
 for subj in subjects:
     ls_run = [] #Lista dove inseriamo le run
@@ -68,13 +72,13 @@ for subj in subjects:
     raw.filter(1.0, 79.0, fir_design='firwin', skip_by_annotation='edge') #Filtro
     raw_notch = raw.notch_filter(freqs=60) #Faccio un filtro passa banda
     #todo: azzerare variabile
-    plot_pre_psd = raw.plot_psd(area_mode=None, show=False, average=False, fmin =1.0, ax=plt.axes(ylim=(0,60)), fmax=80.0, dB=False, n_fft=160)
-    psd_name = os.path.join(dir_pre_psd,'S'+ str(subj) + '_imagined_pre.png')
+    plot_pre_psd = raw.plot_psd(area_mode=None, show=False, average=False, fmin =1.0, fmax=80.0, dB=False, n_fft=160)
+    psd_name = os.path.join(dir_pre_psd,'S'+ str(subj) + '_real_pre.png')
     if os.path.exists(psd_name):
      raise Exception('Warning! This plot already exists! :(' )
     plot_pre_psd.savefig(psd_name)
     # Ica
-    del plot_pre_psd
+    #del plot_pre_psd
     ica = ICA(n_components=64, random_state=42, method="fastica", max_iter=1000)
 
     ind = []
@@ -83,22 +87,41 @@ for subj in subjects:
             ind.append(index)
     (raw.annotations).delete(ind)
 
-    ica.fit(raw)
-    raws.append(raw)
-    icas.append(ica)
 
+    raws_filtered.append(raw)
+    
+    if ica_true == True:
+        ica.fit(raw)
+        raws.append(raw)
+        icas.append(ica)
+    else:
+        continue
 
-corrmap(icas, template=(0, 19), plot=True, threshold=0.80)
-icas[0].plot_properties(raws[0], picks=[26,29,34,44,47,49,51,53,55,56,58,59,63,60,61], dB=False)
+#corrmap(icas, template=(0, 0), plot=True, threshold=0.80)
+#icas[0].plot_properties(raws[0], picks=[26,29,34,44,47,49,51,53,55,56,58,59,63,60,61], dB=False)
 
 #%%
-eog_inds, eog_scores = icas[0].find_bads_eog(raws[0], ch_name='Fpz')
-icas[0].plot_properties(raws[0], picks=[61,62,63], dB=False)
-exc_0 = [0,8,9,10,13,15,18,28,31,32,49,43,44,51,57,]
+#eog_inds, eog_scores = icas[0].find_bads_eog(raws[0], ch_name='Fpz')
+icas[0].plot_properties(raws[0], picks=[0,1,3,17,13,14,17,29,32,33,35,50,47,53], dB=False)
+
+
+#components [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
+#21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,
+#41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60
+# 61,62,63]
+exc_0 = [0,8,9,10,13,15,18,28,31,32,49,43,44,51,57]
 maybe = [ ]
-eyes=
+eyes= [0,]
 ecg=
 movement=
+
+#template from subject 66
+#exc_0_66 = [0,1,3,13,14,17,29,32,33,35,47,50,53]
+eyes = [0,1,3,14,33,35,47]
+#ecg = []
+#movement =[17,32,] 
+#odd = [13]
+#electrode =[53]
 #reconst_raw = raws[0].copy()
 #icas[0].plot_overlay(reconst_raw, exclude=exc_0)
 #reconst_raw.plot_psd(area_mode=None, show=False, average=False, fmin=1.0, fmax=80.0, dB=False, n_fft=160)
@@ -121,7 +144,7 @@ for index, ica in enumerate(icas):
     icas[index].exclude = icas[index].labels_["artifact_0"]
     icas[index].apply(reco_raw)
     plot_post_psd = reco_raw.plot_psd(area_mode=None, show=False, average=False,ax=plt.axes(ylim=(0,60)), fmin=1.0, fmax=80.0, dB=False, n_fft=160)
-    psd_name_post = os.path.join(dir_post_psd, 'S' + str(subjects[index]) + '_imagined_post.png')
+    psd_name_post = os.path.join(dir_post_psd, 'S' + str(subjects[index]) + '_real_post.png')
     if os.path.exists(psd_name_post):
         raise Exception('Warning! This plot already exists! :(')
     plot_post_psd.savefig(psd_name_post)
