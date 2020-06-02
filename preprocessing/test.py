e96fed92484f83a1dd8eaa80e7d9dcd6358b95d1
Francesco Mattioli <Francesco.mttl@gmail.com>
2020-06-02 19:14:39 +0200

    Lots of changes

--- preprocessing/test.py
+++ preprocessing/test.py
@@ -22,6 +22,7 @@ raws_clean = Pirates.del_annotations(raws_filtered)  # Elimino annotazioni
 Pirates.plot_pre_psd(raws_clean, dir_pre_psd, overwrite=True)
 #icas = Pirates.ica_function(raws_clean, dir_icas, save=True)  # Applico una ica
 icas = Pirates.load_saved_icas(dir_icas)
+list_psd = Pirates.get_ica_psd(raws_clean,icas, dir_icas)
 
 eye = [5,0, 32]
 other = [1,2]
@@ -33,64 +34,14 @@ comp_template = eye + other + mov + nb
 corr = Pirates.corr_map(icas, 0, comp_template, dir_templates, "arti", threshold=0.85)
 Pirates.psd_topo_map(icas, raws_clean, "arti", dir_psd_topo_map)
 
+
 #reco_raws = Pirates.reconstruct_raws(icas, raws_clean, "artifact")
 #Pirates.plot_post_psd(reco_raws, dir_post_psd, overwrite=True)
 
 #Pirates.discrepancy(raws_clean,reco_raws,dir_dis)
 #Pirates.create_report_psd(dir_pre_psd, dir_post_psd, dir_dis, dir_report)
 
-from PIL import Image, ImageDraw, ImageFont
-from mne.time_frequency import psd_multitaper, psd_welch
-
-
-
-
 #%%
-
-sources = icas[1].get_sources(raws_clean[1])
-data = sources.get_data(picks=34)
-psds, freqs = plt.psd(data[0], Fs=160)
-plt.close("all")
-plt.plot(freqs,psds)
-
-log_psds = np.sqrt(freqs)
-plt.close("all")
-#dio_aiutaci.reverse()
-a = psds**2/np.sqrt(freqs)
-plt.plot(freqs,a)
-
-
-
-sources = icas[0].get_sources(raws_clean[0])
-data = sources.get_data(picks=34)
-p = []
-f = []
-for comp in n_comp:
-    data = sources.get_data(picks=comp)
-    psds, freqs = plt.psd(data[0], Fs=160, NFFT=160, noverlap=0.6)
-    p.append(psds)
-    f.append(freqs)
-plt.close("all")
-
-for a,b in zip(f,p):
-    #plt.ylim(0, 0.3)
-    plt.plot(a,b)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
