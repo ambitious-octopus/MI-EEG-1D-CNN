4e4f7a7905bba88ff3a8d8e4594ec23c417f1a40
Francesco Mattioli <Francesco.mttl@gmail.com>
2020-07-29 09:37:10 +0200

    Added erds single subjects

--- preprocessing/grand_avarage.py
+++ preprocessing/grand_avarage.py
@@ -125,6 +125,186 @@ for index,raw in enumerate(raws):
 
 grand = mne.grand_average()
 
+#%%
+from pirate import Pirates
+import os
+import numpy as np
+import mne
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import mne
+from mne.datasets import eegbci
+from mne.io import concatenate_raws, read_raw_edf
+from mne.time_frequency import tfr_multitaper
+from mne.stats import permutation_cluster_1samp_test as pcluster_test
+from mne.viz.utils import center_cmap
+
+cwd = os.getcwd()
+p_path = "C:\\Users\\User\\OneDrive\\Documenti\\reco_raws"
+
+raws= list()
+for subdir, dirs, files in os.walk(p_path):
+    for file in files:
+        filepath = subdir + os.sep + file
+        raw = mne.io.read_raw_fif(filepath,preload=True)
+        raws.append(raw)
+
+
+#raw = mne.concatenate_raws(raws[0])
+raw = raws[0]
+
+
+events, _ = mne.events_from_annotations(raw, event_id=dict(T1=2, T2=3))
+picks = mne.pick_channels(raw.info["ch_names"], ["C3", "Cz", "C4"])
+
+# epoch data ##################################################################
+tmin, tmax = -1, 3  # define epochs around events (in s)
+event_ids = dict(left=2, right=3)  # map event IDs to tasks
+
+epochs = mne.Epochs(raw, events, event_ids, tmin - 0.5, tmax + 0.5,
+                    picks=picks, baseline=None, preload=True)
+
+# compute ERDS maps ###########################################################
+freqs = np.arange(2, 35, 1)  # frequencies from 2-35Hz
+n_cycles = freqs  # use constant t/f resolution
+vmin, vmax = -1, 1.5  # set min and max ERDS values in plot
+baseline = [-1, 0]  # baseline interval (in s)
+cmap = center_cmap(plt.cm.RdBu, vmin, vmax)  # zero maps to white
+kwargs = dict(n_permutations=100, step_down_p=0.05, seed=1,
+              buffer_size=None)  # for cluster test
+
+# Run TF decomposition overall epochs
+tfr = tfr_multitaper(epochs, freqs=freqs, n_cycles=n_cycles,
+                     use_fft=True, return_itc=False, average=False,
+                     decim=2)
+tfr.crop(tmin, tmax)
+tfr.apply_baseline(baseline, mode="percent")
+for event in event_ids:
+    # select desired epochs for visualization
+    tfr_ev = tfr[event]
+    fig, axes = plt.subplots(1, 4, figsize=(12, 4),
+                             gridspec_kw={"width_ratios": [10, 10, 10, 1]})
+    for ch, ax in enumerate(axes[:-1]):  # for each channel
+        # positive clusters
+        _, c1, p1, _ = pcluster_test(tfr_ev.data[:, ch, ...], tail=1, **kwargs)
+        # negative clusters
+        _, c2, p2, _ = pcluster_test(tfr_ev.data[:, ch, ...], tail=-1,
+                                     **kwargs)
+
+        # note that we keep clusters with p <= 0.05 from the combined clusters
+        # of two independent tests; in this example, we do not correct for
+        # these two comparisons
+        c = np.stack(c1 + c2, axis=2)  # combined clusters
+        p = np.concatenate((p1, p2))  # combined p-values
+        mask = c[..., p <= 0.05].any(axis=-1)
+
+        # plot TFR (ERDS map with masking)
+        tfr_ev.average().plot([ch], vmin=vmin, vmax=vmax, cmap=(cmap, False),
+                              axes=ax, colorbar=False, show=False, mask=mask,
+                              mask_style="mask")
+
+        ax.set_title(epochs.ch_names[ch], fontsize=10)
+        ax.axvline(0, linewidth=1, color="black", linestyle=":")  # event
+        if not ax.is_first_col():
+            ax.set_ylabel("")
+            ax.set_yticklabels("")
+    fig.colorbar(axes[0].images[-1], cax=axes[-1])
+    fig.suptitle("ERDS ({})".format(event))
+    fig.show()
+
+
+#%%
+from pirate import Pirates
+import os
+import numpy as np
+import mne
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import mne
+from mne.datasets import eegbci
+from mne.io import concatenate_raws, read_raw_edf
+from mne.time_frequency import tfr_multitaper
+from mne.stats import permutation_cluster_1samp_test as pcluster_test
+from mne.viz.utils import center_cmap
+
+cwd = os.getcwd()
+p_path = "C:\\Users\\User\\OneDrive\\Documenti\\reco_raws"
+
+raws= list()
+for subdir, dirs, files in os.walk(p_path):
+    for file in files:
+        filepath = subdir + os.sep + file
+        raw = mne.io.read_raw_fif(filepath,preload=True)
+        raws.append(raw)
+
+
+#raw = mne.concatenate_raws(raws[0])
+# raw = raws[0]
+
+for raw in raws:
+    events, _ = mne.events_from_annotations(raw, event_id=dict(T1=2, T2=3))
+    picks = mne.pick_channels(raw.info["ch_names"], ["C3", "Cz", "C4"])
+
+    # epoch data ##################################################################
+    tmin, tmax = -1, 3  # define epochs around events (in s)
+    event_ids = dict(left=2, right=3)  # map event IDs to tasks
+
+    epochs = mne.Epochs(raw, events, event_ids, tmin - 0.5, tmax + 0.5,
+                        picks=picks, baseline=None, preload=True)
+
+    # compute ERDS maps ###########################################################
+    freqs = np.arange(2, 35, 1)  # frequencies from 2-35Hz
+    n_cycles = freqs  # use constant t/f resolution
+    vmin, vmax = -1, 1.5  # set min and max ERDS values in plot
+    baseline = [-1, 0]  # baseline interval (in s)
+    cmap = center_cmap(plt.cm.RdBu, vmin, vmax)  # zero maps to white
+    kwargs = dict(n_permutations=100, step_down_p=0.05, seed=1,
+                  buffer_size=None)  # for cluster test
+
+    # Run TF decomposition overall epochs
+    tfr = tfr_multitaper(epochs, freqs=freqs, n_cycles=n_cycles,
+                         use_fft=True, return_itc=False, average=False,
+                         decim=2)
+    tfr.crop(tmin, tmax)
+    tfr.apply_baseline(baseline, mode="percent")
+    for event in event_ids:
+        # select desired epochs for visualization
+        tfr_ev = tfr[event]
+        fig, axes = plt.subplots(1, 4, figsize=(12, 4),
+                                 gridspec_kw={"width_ratios": [10, 10, 10, 1]})
+        for ch, ax in enumerate(axes[:-1]):  # for each channel
+            # positive clusters
+            _, c1, p1, _ = pcluster_test(tfr_ev.data[:, ch, ...], tail=1, **kwargs)
+            # negative clusters
+            _, c2, p2, _ = pcluster_test(tfr_ev.data[:, ch, ...], tail=-1,
+                                         **kwargs)
+
+            # note that we keep clusters with p <= 0.05 from the combined clusters
+            # of two independent tests; in this example, we do not correct for
+            # these two comparisons
+            c = np.stack(c1 + c2, axis=2)  # combined clusters
+            p = np.concatenate((p1, p2))  # combined p-values
+            mask = c[..., p <= 0.05].any(axis=-1)
+
+            # plot TFR (ERDS map with masking)
+            tfr_ev.average().plot([ch], vmin=vmin, vmax=vmax, cmap=(cmap, False),
+                                  axes=ax, colorbar=False, show=False, mask=mask,
+                                  mask_style="mask")
+
+            ax.set_title(epochs.ch_names[ch], fontsize=10)
+            ax.axvline(0, linewidth=1, color="black", linestyle=":")  # event
+            if not ax.is_first_col():
+                ax.set_ylabel("")
+                ax.set_yticklabels("")
+        fig.colorbar(axes[0].images[-1], cax=axes[-1])
+        fig.suptitle("ERDS ({})".format(event))
+        fig.savefig("C:\\Users\\User\\OneDrive\\Documenti\\erd\\" + str(raw.__repr__()[7:11]) + str(event) + ".png", dpi="figure")
+        plt.close("all")
+
 
 
 
