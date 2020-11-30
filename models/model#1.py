7db97e95374b1cf84ae3ef9a5e71a7c8dbedc177
Francesco Mattioli <Francesco.mttl@gmail.com>
2020-11-30 20:14:49 +0100

    Added model

--- models/model#1.py
+++ models/model#1.py
@@ -8,10 +8,7 @@ import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
 
-exclude = [38, 88, 89, 92, 100, 104]
-
-#todo: Creare l'input corretto!
-
+#%%
 """
 If Generate and save
 """
@@ -23,19 +20,26 @@ os.mkdir(dir_path)
 s = np.arange(1,86)
 x, y = u.get_data(list(s), [4,6,8,10,12,14], 0, 4, ["C3", "C4"])
 np.save(os.path.join(dir_path, "x_C3_C4"), x, allow_pickle=True)
+np.save(os.path.join(dir_path, "y_C3_C4"), y, allow_pickle=True)
+#%%
+"""
+Load only
+"""
+base_path = "D:"
+dir_path = os.path.join(base_path, "eeg_data")
+
+#Load data
+x_data = np.load(os.path.join(dir_path, "x_C3_C4.npy"))
+y = np.load(os.path.join(dir_path, "y_C3_C4.npy"))
+
 """
 OneHot encoding
 """
-
 total_labels = np.unique(y)
-
 mapping = {}
 for x in range(len(total_labels)):
   mapping[total_labels[x]] = x
-
 for x in range(len(y)):
   y[x] = mapping[y[x]]
-one_hot_encode = keras.utils.to_categorical(y)
+y_data = keras.utils.to_categorical(y)
 
-
-np.save(os.path.join(dir_path, "y_C3_C4"), y, allow_pickle=True)
