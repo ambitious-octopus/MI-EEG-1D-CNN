39eb23d6e93e370f85a964a936b8cc85b28965c7
Francesco Mattioli <Francesco.mttl@gmail.com>
2020-12-07 22:46:11 +0100

    Update model

--- base.py
+++ base.py
@@ -4,15 +4,12 @@ from tensorflow import keras
 import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
-
 # Mi prendo il dataset
 f_mnist = keras.datasets.fashion_mnist
 (x_train_full, y_train_full),(x_test, y_test) = f_mnist.load_data()
-
 # Creo un validation set e scalo tutto
 x_valid, x_train = x_train_full[:5000] / 255.0, x_train_full[5000:] / 255.0
 y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
-
 class_name = ["t-shirt/top", "trouser", "pullover", "dress", "coat", "sandal", "shirt",
               "sneaker", "bag", "ankle boot"]
 
@@ -25,10 +22,8 @@ model.add(keras.layers.Flatten(input_shape=[28,28]))
 model.add(keras.layers.Dense(300, activation="relu"))
 model.add(keras.layers.Dense(100, activation="relu"))
 model.add(keras.layers.Dense(10, activation="softmax"))
-
 # Guardo il modello
 model.summary()
-
 # Compilo il modello
 model.compile(loss="sparse_categorical_crossentropy",
               optimizer=keras.optimizers.SGD(lr=0.01),
