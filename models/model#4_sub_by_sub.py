15d0dd25656d18659393ad86dfc20066c9fcd7c3
Francesco Mattioli <Francesco.mttl@gmail.com>
2021-01-29 21:17:13 +0100

    Added data augmentation

--- models/model#4_sub_by_sub.py
+++ models/model#4_sub_by_sub.py
@@ -14,9 +14,15 @@ assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
 config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
 #############################################################################
 
+# from imblearn.over_sampling import SMOTE
+# sm = SMOTE(random_state=0)
+# XRes, desYRes = sm.fit_sample(X, desY)
+# print('classes count')
+# print ('before oversampling = {}'.format(desY.sum(axis=0)))
+# print ('after oversampling = {}'.format(desYRes.sum(axis=0)))
 
 exclude =  [38, 88, 89, 92, 100, 104]
-subjects = [n for n in np.arange(1,109) if n not in exclude]
+subjects = [n for n in np.arange(1,2) if n not in exclude]
 data_path = "D:\\datasets\\eeg_dataset\\FC3_FC4_sub_no_base_no_filter" #save_path = "D:\\datasets\\eeg_dataset\\C3_C4_sub_no_base_min1_max3" "D:\\datasets\\eeg_dataset\\C3_C4_sub_no_base" "D:\datasets\eeg_dataset\FC3_FC4_sub_no_base_no_filter"
 xs, ys = Utils.load_sub_by_sub(subjects, data_path,"_FC3_FC4_sub_")
 xs, ys = Utils.scale_sub_by_sub(xs, ys)
@@ -33,7 +39,8 @@ y = np.concatenate(new_y)
 x = np.concatenate(xs)
 
 x_resh = x.reshape(x.shape[0], x.shape[2]*x.shape[1])
-x_train, x_test, y_train, y_test = train_test_split(x_resh, y, stratify =y, test_size=0.30,  random_state=17)
+x_train, x_test, y_train, y_test = train_test_split(x_resh, y, stratify =y, test_size=0.30,  random_state=19)
+
 
 #Processing y
 y_train = Utils.to_numerical(y_train, by_sub=True)
@@ -43,53 +50,69 @@ y_test = Utils.to_numerical(y_test, by_sub=True)
 x_train_resh = x_train.reshape(x_train.shape[0], int(x_train.shape[1]/2), 2).astype(np.float64)
 x_test_resh = x_test.reshape(x_test.shape[0], int(x_test.shape[1]/2),2).astype(np.float64)
 
-
 y_train = tf.keras.utils.to_categorical(y_train)
 y_test = tf.keras.utils.to_categorical(y_test)
 
+y_test_arg = np.argmax(y_test, axis=1)
+print('classes count')
+print ('before oversampling = {}'.format(y_test_arg.sum(axis=0)))
+
 #%%
+
+for i in range(len(y)):
+    print(np.count("R0"))
+    print(np.count("L0"))
+    print(np.count("LR0"))
+    print(np.count("F0"))
+
+
+
+
 #Convolution Neural Network
 # [samples, time steps, features].
 # real_x_train = x_train.reshape(14808, 640, 2)
 # real_x_test = x_test.reshape(3703, 640, 2)
-learning_rate = 1e-5 # default 1e-3
-kernel_size = 11
-drop_rate = 0.4 #0.2 good #0.4 local minimum
-
+learning_rate = 1e-3 # default 1e-3
+kernel_size = 2
+drop_rate = 0.3 #0.2 good #0.4 local minimum
 
 
 loss = tf.keras.losses.categorical_crossentropy  #tf.keras.losses.categorical_crossentropy
 optimizer = tf.keras.optimizers.Adam(lr=learning_rate) #tf.keras.optimizers.Adam(lr=learning_rate) tf.keras.optimizers.SGD(learning_rate=learning_rate)
 
+
 model = tf.keras.Sequential()
-model.add(tf.keras.layers.Conv1D(filters=25, kernel_size=kernel_size, strides=1, activation='relu', padding= "valid", input_shape=(640, 2)))
-model.add(tf.keras.layers.SpatialDropout1D(drop_rate))
-model.add(tf.keras.layers.Conv1D(filters=25, kernel_size=2, activation='relu', strides=1, padding= "valid"))
+model.add(tf.keras.layers.Conv1D(filters=10, kernel_size=kernel_size, strides=1, activation='relu', padding= "valid", input_shape=(640, 2)))
 model.add(tf.keras.layers.BatchNormalization())
 model.add(tf.keras.layers.SpatialDropout1D(drop_rate))
-model.add(tf.keras.layers.MaxPool1D(pool_size=3, strides=3))
-model.add(tf.keras.layers.Conv1D(filters=50, kernel_size=kernel_size, activation='relu', strides=1, padding= "valid"))
-model.add(tf.keras.layers.SpatialDropout1D(drop_rate))
-model.add(tf.keras.layers.MaxPool1D(pool_size=3, strides=3))
-model.add(tf.keras.layers.Conv1D(filters=100, kernel_size=kernel_size, activation='relu', strides=1, padding= "valid"))
+model.add(tf.keras.layers.Conv1D(filters=10, kernel_size=5, activation='relu', strides=1, padding= "valid"))
 model.add(tf.keras.layers.BatchNormalization())
+# model.add(tf.keras.layers.SpatialDropout1D(drop_rate))
+# model.add(tf.keras.layers.MaxPool1D(pool_size=3, strides=3))
+model.add(tf.keras.layers.Conv1D(filters=10, kernel_size=kernel_size, activation='relu', strides=1, padding= "valid"))
 model.add(tf.keras.layers.SpatialDropout1D(drop_rate))
-model.add(tf.keras.layers.MaxPool1D(pool_size=3))
-model.add(tf.keras.layers.Conv1D(filters=200, kernel_size=kernel_size, activation='relu', strides=1, padding= "valid"))
-model.add(tf.keras.layers.BatchNormalization())
-model.add(tf.keras.layers.MaxPool1D(pool_size=2))
-model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.AvgPool1D(pool_size=3, strides=3))
+# model.add(tf.keras.layers.Conv1D(filters=50, kernel_size=kernel_size, activation='relu', strides=1, padding= "valid"))
+# model.add(tf.keras.layers.BatchNormalization())
+# model.add(tf.keras.layers.SpatialDropout1D(drop_rate))
+# model.add(tf.keras.layers.MaxPool1D(pool_size=3))
 # model.add(tf.keras.layers.Conv1D(filters=100, kernel_size=kernel_size, activation='relu', strides=1, padding= "valid"))
 # model.add(tf.keras.layers.BatchNormalization())
-model.add(tf.keras.layers.Dense(4, activation='softmax'))
-# model.add(tf.keras.layers.Dense(64, activation='relu'))
+# model.add(tf.keras.layers.AvgPool1D(pool_size=2))
+# model.add(tf.keras.layers.SpatialDropout1D(drop_rate))
+# model.add(tf.keras.layers.Dense(300))
+# model.add(tf.keras.layers.Conv1D(filters=100, kernel_size=kernel_size, activation='relu', strides=1, padding= "valid"))
+# model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Flatten())
+
+model.add(tf.keras.layers.Dense(64, activation='relu'))
 # model.add(tf.keras.layers.Dropout(drop_rate))
 # model.add(tf.keras.layers.Dense(32, activation='relu'))
 # model.add(tf.keras.layers.Dropout(drop_rate))
-# model.add(tf.keras.layers.Dense(4, activation='softmax'))
+model.add(tf.keras.layers.Dense(4, activation='softmax'))
 model.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
 model.summary()
-hist = model.fit(x_train_resh, y_train, epochs=160, batch_size=6, shuffle=True, validation_data=(x_test_resh, y_test))
+hist = model.fit(x_train_resh, y_train, epochs=30, batch_size=32, validation_data=(x_test_resh, y_test))
 
 plt.subplot(1,2,1, title="accuracy")
 plt.plot(hist.history["accuracy"], color="red", label="Train")
