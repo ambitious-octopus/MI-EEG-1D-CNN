dcbfa166ad372fb9c4df4e170b41c961b2d59a9f
Francesco Mattioli <Francesco.mttl@gmail.com>
2021-02-21 11:34:22 +0100

    update model
    89

--- models/model#4_sub_by_sub_ad_base.py
+++ models/model#4_sub_by_sub_ad_base.py
@@ -21,10 +21,9 @@ tf.autograph.set_verbosity(0)
 exclude =  [38, 88, 89, 92, 100, 104]
 subjects = [n for n in np.arange(1,109) if n not in exclude]
 # data_path = "D:\\datasets\\eeg_dataset\\C3_C4_sub"
-data_path = "D:\\datasets\\eeg_dataset\\FC3_FC4_no_filter"
-# "D:\\datasets\\eeg_dataset\\C3_C4_sub_no_base_min1_max3" "D:\\datasets\\eeg_dataset\\C3_C4_sub_no_base" "D:\datasets\eeg_dataset\FC3_FC4_sub_no_base_no_filter"
-sub_name = "_FC3_FC4_sub_"
-# sub_name = "_C3_C4_sub_"
+data_path = "D:\datasets\eeg_dataset\C3_C4_sub"
+sub_name = "_C3_C4_sub_"
+
 xs, ys = Utils.load_sub_by_sub(subjects, data_path,sub_name)
 xs, ys = Utils.scale_sub_by_sub(xs, ys)
 
@@ -47,17 +46,17 @@ print('classes count')
 print ('before oversampling = {}'.format(y_real.sum(axis=0)))
 
 from imblearn.over_sampling import SMOTE
-sm = SMOTE(random_state=0)
+sm = SMOTE(random_state=42)
 XRes, desYRes = sm.fit_sample(x_resh, y_real)
 print('classes count')
 print ('before oversampling = {}'.format(y_real.sum(axis=0)))
 print ('after oversampling = {}'.format(desYRes.sum(axis=0)))
 x_train, x_test, y_train, y_test = train_test_split(XRes, desYRes, stratify=desYRes,
                                                     test_size=0.20,
-                                                    random_state=19)
+                                                    random_state=42)
 x_valid, x_test, y_valid, y_test = train_test_split(x_test, y_test, stratify=y_test,
                                                     test_size=0.50,
-                                                    random_state=17)
+                                                    random_state=42)
 
 #Processing y
 # y_train = Utils.to_numerical(y_train, by_sub=True)
@@ -77,30 +76,32 @@ x_valid_resh = x_valid.reshape(x_valid.shape[0], int(x_valid.shape[1]/2),2).asty
 # real_x_train = x_train.reshape(14808, 640, 2)
 # real_x_test = x_test.reshape(3703, 640, 2)
 learning_rate = 1e-4 # default 1e-3
-kernel_size_0 = 7 #5 e 6 good learning_rate = 1e-4 good
-kernel_size_1 = 3
-drop_rate = 0.45 #0.2 good #0.4 local minimum
+kernel_size_0 = 6 #5 e 6 good learning_rate = 1e-4 good
+kernel_size_1 = 4
+drop_rate = 0.4 #0.2 good #0.4 local minimum
 
 
 loss = tf.keras.losses.categorical_crossentropy  #tf.keras.losses.categorical_crossentropy
 optimizer = tf.keras.optimizers.Adam(lr=learning_rate)
 # optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate)
 model = tf.keras.Sequential()
-model.add(tf.keras.layers.Conv1D(filters=64, kernel_size=kernel_size_0,
+model.add(tf.keras.layers.Conv1D(filters=32, kernel_size=kernel_size_0,
                                  activation='relu', padding= "same", input_shape=(640, 2)))
+model.add(tf.keras.layers.SpatialDropout1D(drop_rate))
 # model.add(tf.keras.layers.BatchNormalization())
-# model.add(tf.keras.layers.AvgPool1D(pool_size=2))
-model.add(tf.keras.layers.MaxPool1D(pool_size=2))
-
-model.add(tf.keras.layers.Conv1D(filters=128, kernel_size=kernel_size_1, activation='relu',
-                                 padding= "valid"))
-model.add(tf.keras.layers.Conv1D(filters=128, kernel_size=kernel_size_1, activation='relu',
+# model.add(tf.keras.layers.MaxPool1D(pool_size=2))
+model.add(tf.keras.layers.Conv1D(filters=64, kernel_size=kernel_size_1, activation='relu',
                                  padding= "valid"))
 # model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.SpatialDropout1D(drop_rate))
+model.add(tf.keras.layers.MaxPool1D(pool_size=2))
+model.add(tf.keras.layers.Conv1D(filters=64, kernel_size=kernel_size_1, activation='relu',
+                                 padding= "valid"))
+model.add(tf.keras.layers.SpatialDropout1D(drop_rate))
 model.add(tf.keras.layers.MaxPool1D(pool_size=2))
 
 
-# model.add(tf.keras.layers.Conv1D(filters=15, kernel_size=kernel_size, activation='relu',
+# model.add(tf.keras.layers.Conv1D(filters=8, kernel_size=kernel_size_1, activation='relu',
                                  # padding= "valid"))
 # model.add(tf.keras.layers.BatchNormalization())
 # model.add(tf.keras.layers.Dropout(drop_rate))
@@ -113,9 +114,9 @@ model.add(tf.keras.layers.MaxPool1D(pool_size=2))
 # model.add(tf.keras.layers.Conv1D(filters=100, kernel_size=kernel_size, activation='relu', strides=1, padding= "valid"))
 # model.add(tf.keras.layers.BatchNormalization())
 model.add(tf.keras.layers.Flatten())
-model.add(tf.keras.layers.Dense(64, activation='relu'))
+model.add(tf.keras.layers.Dense(256, activation='relu'))
 model.add(tf.keras.layers.Dropout(drop_rate))
-model.add(tf.keras.layers.Dense(32, activation='relu'))
+model.add(tf.keras.layers.Dense(64, activation='relu'))
 model.add(tf.keras.layers.Dropout(drop_rate))
 model.add(tf.keras.layers.Dense(5, activation='softmax'))
 model.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
@@ -135,12 +136,12 @@ checkpoint = ModelCheckpoint( # set model saving checkpoints
 earlystopping = EarlyStopping(
     monitor='val_loss', # set monitor metrics
     min_delta=0.001, # set minimum metrics delta
-    patience=50, # number of epochs to stop training
+    patience=10, # number of epochs to stop training
     restore_best_weights=True, # set if use best weights or last weights
     )
 callbacksList = [checkpoint, earlystopping] # build callbacks list
 
-hist = model.fit(x_train_resh, y_train, epochs=60, batch_size=2,
+hist = model.fit(x_train_resh, y_train, epochs=60, batch_size=10,
                  validation_data=(x_valid_resh, y_valid), callbacks=callbacksList) #32
 
 
