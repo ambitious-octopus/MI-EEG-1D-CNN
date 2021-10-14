b3d3ca6c7431f944d0b0fcb5ad85b9f8fb4cf248
Francesco Mattioli <francesco.mttl@gmail.com>
2021-10-14 16:04:36 +0200

    added docker, comment lstm

--- dataset_generator/1second_sequential_generator.py
+++ dataset_generator/1second_sequential_generator.py
@@ -2,8 +2,6 @@ import os
 import sys
 import numpy as np
 from data_processing.general_processor import Utils
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import minmax_scale
 import pickle
 import mne
 from mne.io import read_raw_edf
@@ -11,6 +9,10 @@ from mne.datasets import eegbci
 from mne.channels import make_standard_montage
 from mne.epochs import Epochs
 import pandas as pd
+
+WINDOW_LEGHT = 80
+# Carefoul EPOCH_DIVISON should be int
+EPOCH_DIVISON = int(640/WINDOW_LEGHT)
 
 #Select channel combination
 channels = Utils.combinations["e"]
@@ -36,8 +38,9 @@ data_path = "E:\\datasets\\eegbci"
 save_path = "E:\\datasets\\eegnn\\seq"
 
 
-task2 = [4, 8, 12]
-task4 = [6, 10, 14]
+task2 = [4, 8, 12] #(imagine opening and closing left or right fist)
+task4 = [6, 10, 14] #(imagine opening and closing both fists or both feet)
+# Get files
 for subject in subjects:
     if len(subject) == 1:
         sub_name = "S" + "00" + subject
@@ -45,16 +48,24 @@ for subject in subjects:
         sub_name = "S" + "0" + subject
     else:
         sub_name = "S" + subject
+    #Once we build the subject name adddres we join it with full path of the dataset
     sub_folder = os.path.join(data_path, sub_name)
+    #At this point we should take the runs
     for run in runs:
         if len(run) == 1:
             path_run = os.path.join(sub_folder, sub_name + "R" + "0" + run + ".edf")
         else:
             path_run = os.path.join(sub_folder, sub_name + "R" + run + ".edf")
-        raw_run = read_raw_edf(path_run, preload=True)  # Le carico
-        eegbci.standardize(raw_run)  # Cambio n_epoch nomi dei canali
-        montage = make_standard_montage('standard_1005')  # Caricare il montaggio
-        raw_run.set_montage(montage)  # Setto il montaggio
+        # Load the raw edf of a single subject and a single run
+        raw_run = read_raw_edf(path_run, preload=True)
+        # Simply change the channels names ".C3" -> "C3"
+        eegbci.standardize(raw_run)
+        # Load the correct montage
+        montage = make_standard_montage('standard_1005')
+        # Apply correct montage to raw_edf file
+        raw_run.set_montage(montage)
+        # Check if the run is in task 2 or task 4
+        # aka -> (imagine opening and closing both fists or both feet) or (imagine opening and closing left or right fist)
         if int(run) in task2:
             for index, an in enumerate(raw_run.annotations.description):
                 if an == "T0":
@@ -63,6 +74,7 @@ for subject in subjects:
                     raw_run.annotations.description[index] = "L"
                 if an == "T2":
                     raw_run.annotations.description[index] = "R"
+
         if int(run) in task4:
             for index, an in enumerate(raw_run.annotations.description):
                 if an == "T0":
@@ -71,8 +83,8 @@ for subject in subjects:
                     raw_run.annotations.description[index] = "LR"
                 if an == "T2":
                     raw_run.annotations.description[index] = "F"
+        # For each couple of channels (e.g. ["FC1", "FC2"])
         for channel in channels:
-            # raw = raw_run.pick_channels(channel)
             raw = raw_run.copy()
             if int(run) in task2:
                 event_id = dict(B=1, L=2, R=3)
@@ -80,11 +92,13 @@ for subject in subjects:
                 event_id = dict(B=1, LR=2, F=3)
             else:
                 raise Exception("Screeeeammmmm")
+
             tmin = 0
             tmax = 4
             events, _ = mne.events_from_annotations(raw, event_id=event_id)
 
-            picks = mne.pick_types(raw.info, meg=False, eeg=True, stim=False, eog=False,
+            picks = mne.pick_types(raw.info, meg=False,
+                                   eeg=True, stim=False, eog=False,
                                    exclude='bads', selection=channel)
 
             epochs = Epochs(raw, events, event_id, tmin, tmax, proj=True, picks=picks,
@@ -93,18 +107,21 @@ for subject in subjects:
             finalx = list()
             finaly = list()
             for index, i in enumerate(epochs):
-                counter = 80
-                for a in range(8):
+                counter = WINDOW_LEGHT
+                for a in range(EPOCH_DIVISON):
                     if a == 0:
                         finalx.append(i[:, :counter])
-                        counter += 80
+                        counter += WINDOW_LEGHT
                     else:
-                        finalx.append(i[:, counter- 80 :counter])
+                        finalx.append(i[:, counter - WINDOW_LEGHT :counter])
                 finaly.append(epochs[index]._name)
             encoded = list()
 
+            # Transform the categorical label (e.g. "B") to one-hot (e.g. [1,0,0,0,0])
             for i in finaly:
                 encoded.append(map[i])
+            # todo: save epoch by epoch in order to feed the tf.data.Dataset.from_generator
+            
 
             x = np.stack(finalx)
             y = np.stack(encoded)
